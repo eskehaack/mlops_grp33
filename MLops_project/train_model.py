5a8fe7ebe37d783799d1af902467caab9a0687cf
Magdk01 <Magnusvinjebo@hotmail.com>
2024-01-08 09:44:28 +0100

    wandb working prototype integration

--- MLops_project/train_model.py
+++ MLops_project/train_model.py
@@ -1,6 +1,17 @@
 from pytorch_lightning import Trainer
 from models.model import VGG
-
+import pytorch_lightning as pl
+import torch
+torch.manual_seed(42)
 model = VGG(101)
-trainer = Trainer(max_epochs=1, limit_train_batches=0.05)
+size_limiter = 0.05
+trainer = Trainer(
+    max_epochs=2,
+    limit_train_batches=size_limiter,
+    limit_test_batches=size_limiter,
+    limit_val_batches=size_limiter,
+    logger=pl.loggers.WandbLogger(project="dtu_mlops", name="test_run"),
+)
 trainer.fit(model)
+
+trainer.test(model)
