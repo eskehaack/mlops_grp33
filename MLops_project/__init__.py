a9c15c913208cbfe7714a735a80f827b85294b76
eskehaack <eskehaack@gmail.com>
2024-01-11 16:08:31 +0100

    added standerdize function to init

--- MLops_project/__init__.py
+++ MLops_project/__init__.py
@@ -1,3 +1,3 @@
 from MLops_project.models.model import VGG
 from MLops_project.data.dataload import food101_dataloader
-from MLops_project.data.process_data import load_statistics
+from MLops_project.data.process_data import load_statistics, calculate_mean_std
