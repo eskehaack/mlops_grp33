286bb39a8d939f8adfc3d4735e94d932d37c8bf3
eskehaack <eskehaack@gmail.com>
2024-01-11 16:51:52 +0100

    ruff formatting

--- tests/test_data.py
+++ tests/test_data.py
@@ -11,6 +11,12 @@ from data_check import data_check
     reason="Data files not found",
 )
 def test_food101_dataloader():
+    """
+    Testing of the data loader.
+    This test checks if:
+        The output of the dataloader has the correct format.
+        The batching works properly.
+    """
     bt_size = 64
     data = food101_dataloader(batch_size=bt_size)
     assert len(data) == 3, "Dataloader function did not return 3 objects"
@@ -27,6 +33,13 @@ def test_food101_dataloader():
     reason="Data files not found",
 )
 def test_calculate_mean_std():
+    """
+    Testing of the standerdization.
+    This test checks if:
+        The output of the standerdizer is the correct type.
+        The output of the standerdizer has the correct keys.
+        The output of the standerdizer has the correct dimension.
+    """
     keys = ["mean", "std"]
     parameters = calculate_mean_std()
     assert parameters.isinstance(dict), f"Parameter output is not of type dict: {type(parameters)}"
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,5 +1,6 @@
 import pytest
 from torch import ones, Tensor, LongTensor
+import numpy as np
 
 from MLops_project import VGG
 from data_check import data_check
@@ -13,6 +14,11 @@ DATA = ones((1, 3, 64, 64))
     reason="Data files not found",
 )
 def test_VGG_output():
+    """
+    Testing of the model.
+    This test checks if:
+        The model outputs the data in the correct dimension.
+    """
     out_dim = 101
     inp_data = DATA
     model = VGG(out_dim, 64, 4, 0.001)
@@ -29,14 +35,21 @@ def test_VGG_output():
     reason="Data files not found",
 )
 def test_VGG_training():
+    """
+    Testing of the training function.
+    This test checks if:
+        The training function can produce a loss.
+        The training does not produce a NAN or NAN-like loss
+        The training loss is zero or positive
+    """
     out_dim = 101
     inp_data = DATA
     model = VGG(out_dim, 64, 4, 0.001)
     loss = model.training_step((inp_data, LongTensor([1])), Tensor([1]))
 
     float(loss)  # If this fails, the test is failed
-
-    assert loss > 0, f"Loss less than zero: {loss}"
+    assert not np.isnan(loss)
+    assert loss >= 0, f"Loss less than zero: {loss}"
 
 
 @pytest.mark.skipif(
@@ -44,6 +57,13 @@ def test_VGG_training():
     reason="Data files not found",
 )
 def test_VGG_validation():
+    """
+    Testing of the validation function.
+    This test checks if:
+        The validation function can produce a loss.
+        The validation does not produce a NAN or NAN-like loss
+        The validation loss is zero or positive
+    """
     out_dim = 101
     inp_data = DATA
     model = VGG(out_dim, 64, 4, 0.001)
@@ -59,6 +79,13 @@ def test_VGG_validation():
     reason="Data files not found",
 )
 def test_VGG_test():
+    """
+    Testing of the test function.
+    This test checks if:
+        The test function can produce a loss.
+        The test does not produce a NAN or NAN-like loss
+        The test loss is zero or positive
+    """
     out_dim = 101
     inp_data = DATA
     model = VGG(out_dim, 64, 4, 0.001)
