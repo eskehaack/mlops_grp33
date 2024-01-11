bc499401f2268424c37cc9e12bcaf427aa49e98d
eskehaack <eskehaack@gmail.com>
2024-01-11 12:00:07 +0100

    fixed data check for tests

--- tests/data_check.py
+++ tests/data_check.py
@@ -2,14 +2,13 @@ import os
 
 
 def data_check():
-    parent_path_check = all(
+    file_check = all(
         [
             os.path.exists("data/processed/test"),
             os.path.exists("data/processed/train"),
             os.path.exists("data/processed/val"),
         ]
-    )
-    subpath_check = all(
+    ) and all(
         [
             len(os.listdir("data/processed/test/")) >= 1,
             len(os.listdir("data/processed/train/")) >= 1,
@@ -17,4 +16,4 @@ def data_check():
         ]
     )
 
-    return all([check for check in [parent_path_check, subpath_check]])
+    return file_check
